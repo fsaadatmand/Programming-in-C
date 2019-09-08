a83d079cc80cfe059abffe7b87f737ecd77b63b1
cdude <cdude996@gmail.com>
2019-09-08 17:51:56 -0400

    initials sum

--- chapter17/ex1/17.4.c
+++ chapter17/ex1/17.4.c
@@ -5,12 +5,9 @@
 int main(void) 
 {
 	const int data[5] = { 1, 2, 3 , 4, 5 };
-	int i, sum;
-
+	int i, sum = 0;
 	for (i = 0; i >= 0; ++i)
 		sum += data[i];
-
 	printf("sum = %i\n", sum);
-
 	return 0;
 }
