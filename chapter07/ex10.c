c5a2c96fc01a246e790d1471bdda0b21364cb1a8
cdude <cdude996@gmail.com>
2019-10-12 14:08:08 -0400

    cleaned up the code

--- chapter07/ex10.c
+++ chapter07/ex10.c
@@ -1,41 +1,37 @@
 /* 
  * 10. Write a function prime() that returns 1 if its argument is a prime
  * number and returns 0 otherwise.
+ *
  * By Faisal Saadatmand
  */
 
 #include <stdio.h>
 #include <stdbool.h>
 
-int prime(int number)
+bool prime(int number);
+
+bool prime(int number)
 {
 	int i;
-	bool isPrime = true;
 
-	if (number == 2)    /* exclude 2 otherwise it will be wrongly flaged as 
-						   an even number */ 
-		isPrime = true;
-	else if (number < 2 || number % 2 == 0)    /* exclude negative numbers,
-												  0, 1 and even numbers */
-			isPrime = false;
-	else  
-		for (i = 3; i < number - 1; ++i) 	
-			if (number % i == 0)
-				return 0;
-			else
-				isPrime = true;	
-	
-	return isPrime;
+   /* Exclude all numbers under 2 and all even numbers.
+	* 2 is a special case: though it's even, it's a prime. */
+	if (number != 2 && (number < 2 || number % 2 == 0))
+		return false;
+	/* check if number is divisible by any number other than itself */
+	for (i = 3; i < number - 1; ++i) 	
+		if (number % i == 0)
+			return false;
+
+	return true;
 }
 
 int main (void)
 {
 	int number;
-	int prime(int number);
 	
-	printf("Enter number to test for  primality: ");
+	printf("Enter number to check if it is a prime number: ");
 	scanf("%i", &number);
-
 	printf("%i\n", prime(number));
 	
 	return 0;
