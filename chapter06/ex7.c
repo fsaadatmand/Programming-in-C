7faeb7cfb7f69f4e0383debfab114c42746ca756
cdude <cdude996@gmail.com>
2019-10-14 13:53:30 -0400

    complete rewrite
    The previous version was inefficient for it was badly written. This I
    believe is a lot better.

--- chapter06/ex7.c
+++ chapter06/ex7.c
@@ -5,15 +5,15 @@
  * numbers up to n = 150. What can you say about this algorithm as compared to
  * the ones used in the text for calculating prime numbers?
  *
- * Sieve of Eratosthenes Algorithm
- * To Display All Prime Numbers Between 1 and n
- * Step 1: Define an array of integers P. Set all elements Pi to 0, 2 <= i
- *         <= n.
- * Step 2: Set i to 2.
- * Step 3: If i > n, the algorithm terminates.
- * Step 4: If Pi is 0, then i is prime.
- * Step 5: For all positive integer values of j, such that i x j ≤ n, set Pixj
- *         to 1.
+ *	Sieve of Eratosthenes Algorithm
+ *	To Display All Prime Numbers Between 1 and n
+ *	Step 1: Define an array of integers P. Set all elements Pi to 0,
+ *			2 <= i <= n.
+ *	Step 2: Set i to 2.
+ *	Step 3: If i > n, the algorithm terminates.
+ *	Step 4: If Pi is 0, then i is prime.
+ *	Step 5: For all positive integer values of j, such that i x j ≤ n,
+ *			set Pixj to 1.
  *
  * By Faisal Saadatmand
  */
@@ -21,40 +21,34 @@
 #include <stdio.h>
 
 int main(void) {
+	/* Step 1: define an array of integers P. Set all elements of P[i] to 0,
+	 * 2 <= i <= n */
+	size_t i, j;  /* size_t is unsigned long */
+	int P[1000001]; /* 151 because the array's index starts at 0 */
+	const size_t n = 1000000;
 
-/* Step 1: define an array of integers P. Set all elements to 
- * P[i] to 0, 2 <= i <= n */
-
-	int  P[151], N[151], i, j, n;
-
-	n = 150;
-
-	for (i = 2; i <= n; ++i) {
+	for (i = 2; i <= n; ++i)
 		P[i] = 0;
-		N[i] = i;
-	}
 
-/* Step 2: set i to 2 */
+	/* Step 2: set i to 2 */
 	i = 2;
-
-/* Step 3: If i > n, the algorithm terminates */
+	/* Step 3: If i > n, the algorithm terminates */
 	while (i < n) {
-
-/* Step 4: If P[i] is 0, then i is prime */
-		if (P[i] == 0)
-			printf("%i ", N[i]);
-
-/* Step 5: For all positive integer values of j, such
- * i x j <= n, set P[i x j] to 1. */
-
-		for (j = 2; j <= n; ++j)
-			if (i * j <= n)
-				P[i*j] = 1;
-
-/* Step 6: Add 1 to i and got to step 3. */
-	++i;
+		/* Step 4: If P[i] is 0, then i is prime */
+		if (P[i] == 0) {
+			/* Step 5: For all positive integer values of j, such i x j <= n,
+			 * set P[i x j] to 1. NOTE: j is all the multiples of i. */
+			for (j = i * i; j <= n; j += i)
+				P[j] = 1;
+		}
+		/* Step 6: Add 1 to i and go to step 3. */
+		++i;
 	}
 
+	/* Print prime numbers */
+	for (i = 0; i <= n; ++i)
+		if(!P[i])
+			printf(" %lu", i);
 	printf("\n");
 
 	return 0;
