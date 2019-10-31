4ea93cbafd1ee1b25f3dae91738ef369c679fc85
cdude <cdude996@gmail.com>
2019-10-31 13:43:20 -0400

    rewrite
    Corrected the answer and cleaned up the code. The previous solution was
    wrong.

--- chapter11/ex2.c
+++ chapter11/ex2.c
@@ -2,36 +2,41 @@
  * 2. Write a program that determines whether your particular computer performs
  * an arithmetic or a logical right shift.
  *
- * Note: reverse the return values to create isLogicalRS() function instead.
- * You could also use retun values 0 and 1 if you did not want to use
- * stdbool.h. In which case, replace the if statement's condition with: 
- * if (isArithmetic == 1)
- *     ... 
- * else
- *     ...
- *
  * By Faisal Saadatmand
  */
 
 #include <stdio.h>
 #include <stdbool.h>
 
+#define MAXLEN 16
+
+/* check for 1 in the most significant bit */
 bool isArithmeticRS(void) 
 {
-	signed int w = -1;
+	signed int w = 0x0;
 
-	w >>= 1;               /* Shift 1 bit to the right */
-	/* Test the value of the most significant bit */
-	if (w < 0)             /* 1 was pushed into MSB */
-		return true;
-	else if (w > 0)        /* 0 was pushed into MSB */
-		return false;
+	w >>= 1;
+
+	if (w >= 0x0)
+		return	false; /* 0 was pushed into MSB */
+
+	return true; /* 1 was pushed into MSB */
+}
+
+/* Alternatively you cancheck for 0 in the most significant bit */
+bool isLogicalRS(void)
+{
+	signed int w = 0x0;
+
+	w >>= 1;
+
+	return (w < 0x0) ? false : true;
 }
 
 int main(void)
 {
-	if (isArithmeticRS)
-		printf("Your computer performs an arithmetic right shift\n");
-	else
-		printf("Yoour computer performs a logical right shift\n");
+	printf("Your computer performs %s right shift\n",
+			isArithmeticRS() ? "an arithmetic" : "a logical");
+
+	return 0;
 }
