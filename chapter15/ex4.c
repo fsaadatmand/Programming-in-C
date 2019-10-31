778af716d16f96281c067c8230f26bd924426219
cdude <cdude996@gmail.com>
2019-10-30 22:16:58 -0400

    code clean up

--- chapter15/ex4.c
+++ chapter15/ex4.c
@@ -2,47 +2,48 @@
  * 4. Write a program that merges lines alternately from two files and writes
  * the results to stdout. If one file has fewer lines than the other, the
  * remaining lines from the larger file should simply be copied to stdout.
+ *
  * By Faisal Saadatmand
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 
+#define NAME_MAXLEN 64
+#define MAXLEN 1000
+
 int main(void) 
 {
-	char name1[64], name2[64];
+	char name1[NAME_MAXLEN], name2[NAME_MAXLEN];
+	char line[MAXLEN];
 	FILE *file1, *file2;
-	char line[81];
 	
 	printf("Merge lines from two files alternately.\n");
 
-	/* Get the names from user */
 	printf("Enter name of file 1: ");
 	scanf("%63s", name1);
 
 	printf("Enter name of file 2: ");
 	scanf("%63s", name2);
 
-	/* Open files */
-	if ((file1 = fopen(name1, "r")) == NULL) {
+	if (!(file1 = fopen(name1, "r"))) {
 		printf("Can't open %s for reading.\n", name1);
 		exit (EXIT_FAILURE);
 	}
 
-	if ((file2 = fopen(name2, "r")) == NULL) {
+	if (!(file2 = fopen(name2, "r"))) {
 		printf("Can't open %s for reading.\n", name2);
 		exit (EXIT_FAILURE);
 	}
 
 	/* Read line per line using fgets() and print to stdout using printf() */
-	while (!feof(file1) || !feof(file2)) {  /* Test for end-of-file condition */
-		if (fgets(line, 80, file1) != NULL) /* extract and print lines */
+	while (!feof(file1) || !feof(file2)) {
+		if (fgets(line, MAXLEN, file1))
 			printf("%s", line);             
-		if (fgets(line, 80, file2) != NULL)
+		if (fgets(line, MAXLEN, file2))
 			printf("%s", line);
 	}
 
-	/* Close open files */
 	fclose(file1);
 	fclose(file2);
 
