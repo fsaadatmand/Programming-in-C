8976f20807a6ffbfe1eb3b40dcb702ace0370604
cdude <cdude996@gmail.com>
2019-10-30 22:27:58 -0400

    code cleanup

--- chapter15/ex3.c
+++ chapter15/ex3.c
@@ -8,11 +8,13 @@
 #include <ctype.h>
 #include <stdlib.h>
 
+#define NAME_MAXLEN 64
+
 int main(void) 
 {
-	char inName[64], outName[64];
-	FILE *in, *out;
+	char inName[NAME_MAXLEN], outName[NAME_MAXLEN];
 	int  c;
+	FILE *in, *out;
 
 	/* Get file name from user */
 	printf("Enter name of the file to be copied: ");
@@ -21,27 +23,23 @@ int main(void)
 	printf("Enter name of the output file: ");
 	scanf("%64s", outName);
 
-	/* Open input and output files */
-	in = fopen(inName, "r");
-	if (in == NULL) {
-		printf("Can't open %s for reading.\n", inName);
+	/* Open input file */
+	if (!(in = fopen(inName, "r"))) {
+		fprintf(stderr, "Can't open %s for reading.\n", inName);
 		exit(EXIT_FAILURE);
 	}
 
-	out = fopen(outName, "w");
-	if (out == NULL) {
-		printf("Can't open %s for writing.\n", outName);
+	/* Open output file */
+	if (!(out = fopen(outName, "w"))) {
+		fprintf(stderr, "Can't open %s for writing.\n", outName);
 		exit(EXIT_FAILURE);
 	}
 
 	/* Copy in to out */
-	while ((c = getc(in)) != EOF) {
-		if (islower(c))
-			c = toupper(c);        /* Convert to uppercase */
-		putc(c, out);
-	}
+	while ((c = getc(in)) != EOF)
+		putc(toupper(c), out);
 
-	/* close open files */
+	/* Close open files */
 	fclose(in);
 	fclose(out);
 
