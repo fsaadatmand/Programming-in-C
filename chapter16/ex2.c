84550f6838c61f6565948d15e3f815fd16443937
cdude <cdude996@gmail.com>
2019-10-30 22:59:45 -0400

    code clean up

--- chapter16/ex2.c
+++ chapter16/ex2.c
@@ -11,10 +11,14 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define MAXLEN 100
+
 struct entry {
-	char word[15];
-	char definition[50];
+	char word[MAXLEN];
+	char definition[MAXLEN];
 };
+
+int lookup(const struct entry [], const char [], const int);
 
 int lookup(const struct entry dictionary[], const char search[],
 		const int entries)
@@ -22,7 +26,7 @@ int lookup(const struct entry dictionary[], const char search[],
 	int i;
 
 	for (i = 0; i < entries; ++i)
-		if (strcoll(search, dictionary[i].word) == 0)
+		if (!strcmp(search, dictionary[i].word))
 			return i;
 
 	return -1;
@@ -30,33 +34,31 @@ int lookup(const struct entry dictionary[], const char search[],
 
 int main(int argc, char *argv[]) 
 {
-	const struct entry dictionary[100] =
-	{ { "aardvark", "a burrowing African mammal"        },
-	  { "abyss",    "a bottomless pit"                  },
-	  { "acumen",   "mentally sharp; keen"              },
-	  { "addle",    "to become confused"                },
-	  { "aerie",    "a high nest"                       },
-	  { "agar",     "a jelly made from seaweed"         },
-	  { "ahoy",     "a nautical call of greeting"       },
-	  { "aigrette", "an ornamental cluster of feathers" },
-	  { "ajar",     "partially opened"                  } };
+	const struct entry dictionary[] =
+	  { { "aardvark", "a burrowing African mammal"        },
+		{ "abyss",    "a bottomless pit"                  },
+		{ "acumen",   "mentally sharp; keen"              },
+		{ "addle",    "to become confused"                },
+		{ "aerie",    "a high nest"                       },
+		{ "agar",     "a jelly made from seaweed"         },
+		{ "ahoy",     "a nautical call of greeting"       },
+		{ "aigrette", "an ornamental cluster of feathers" },
+		{ "ajar",     "partially opened"                  } };
 
-	int entries = 10;
+	int entries = sizeof(dictionary) / sizeof(dictionary[0]);
 	int entryNumber;
-	int lookup(const struct entry dictionary[], const char search[],
-			   const int entries);
-	
+
 	if (argc != 2) {
 		fprintf(stderr, "No word typed on the command line.\n");
 		return EXIT_FAILURE;
 	}
 
 	entryNumber = lookup(dictionary, argv[1], entries);
-		
-		if (entryNumber != -1)
-			printf("%s\n", dictionary[entryNumber].definition);
-		else
-			printf("Sorry, %s is not in my dictionary.\n", argv[1]);
 
-		return EXIT_SUCCESS;
+	if (entryNumber != -1)
+		printf("%s\n", dictionary[entryNumber].definition);
+	else
+		printf("Sorry, %s is not in my dictionary.\n", argv[1]);
+
+	return EXIT_SUCCESS;
 }
