c14f2ca1e90097c63f5f709a4a5c3428dc8973f8
cdude <cdude996@gmail.com>
2019-11-01 14:56:54 -0400

    rewrite

--- chapter11/ex5.c
+++ chapter11/ex5.c
@@ -11,86 +11,69 @@
 
 #include <stdio.h>
 
-/* Function that counts the number of bits contained in a given integer */
-int int_size(int w)
+/* functions */
+size_t int_size();
+size_t bit_test(unsigned int, size_t);
+unsigned int bit_set(unsigned int, size_t);
+
+size_t int_size()
 {
-	int mask, nBits = 0, nibble = 4;
-	
-	mask = ~0;                     /* Integer full of 1's */
-	if (w >= 0) {
-		w = ~w;                    /* One's complement to invert the bits */
-		while (w != mask) {
-			w >>= 1;               /* Right shift until w equals mask */
-			++nBits;
-		}
-		if (nBits % nibble !=0)    /* Ensure each nibble is 4 bits */
-			nBits += nibble - nBits % nibble;
-	} else if (w < 0) {            /* Negative integers */
-		w |= mask;                 /* Set all bits to 1 */
-		while (w != 0) {           /* Left shift until w equal zero */
-			w <<= 1;          
-			++nBits;
-		}
+	int x = ~0;
+	size_t i = 0;
+
+	while (x != 0x0) {
+		x <<= 1;
+		++i;
 	}
 
-	return nBits;
+	return i;
 }
 
 /* Function to check the On/Off status of a given bit inside an integer */
-int bit_test(unsigned int w, int n)
+size_t bit_test(unsigned int w, size_t n)
 {
-	int msb, bit; 
+	size_t offset, mask;
 
-	msb = int_size(w) - 1;      /* Location of most significant bit in w,
-								   i.e. bit 0 */
+	/* location of n starting from the least significant bit */
+	offset = int_size() - 1 - n;
 
-	if ((n < 0) | (n > msb))        /* Check if n is inside the word (w) */
-		return -1;
+	mask = 0x1 << offset;
 
-	bit = msb - n;              /* Location of the bit to be tested */
-	w = w >> bit & 0x1;         /* Extract the bit */
-
-	if (w == 0x1)               /* Test the status of the bit */
-		return 1;               /* Bit is ON */
-	else
-		return 0;               /* Bit is OFF */
+    /* Extract and return the bit */
+	return w & mask;
 }
 
 /* Function to set the status of a bit to On inside an integer */
-unsigned int bit_set(unsigned int w, int n)
+unsigned int bit_set(unsigned int w, size_t n)
 {
-	int msb, bit; 
+	size_t offset, mask;
 
-	msb = int_size(w) - 1;     /* Location of most significant bit in w,
-	                              i.e. bit 0 */
-	bit = msb - n;             /* Location of the bit to change */
+	/* location of n starting from the least significant bit */
+	offset = int_size() - 1 - n;
 
-	w |=  0x1 << bit;          /* Insert 1 into w at the location of var bit */
+	mask = 0x1 << offset;
 
-	return w;
+	// turn on bit n and return value 
+	return  w | mask;
 }
 
 int main(void) 
 {
-	int int_size(int w);
-	int bit_test(unsigned int w, int n);
-	unsigned int bit_set(unsigned int w, int n);
+	unsigned int w = 0xfffff0f0u;
+	const size_t bit = 0; /* least significant bit, i.e. rightmost bit */
 
-	unsigned int w = 0xff0u;
-	int n = 8, result;         /* n is the value of the bit to be tested or
-								  change */
-	
-	result = bit_test(w, n);
+	/* out of bounds check */
+	if (bit < 0 ||  bit > int_size() - 1) {
+		printf("Bit number is outside of word-size bounds\n");
+		return -1;
+	}
 
-	printf("w = %x\n", w);
+	 /* Test the state of the bit */
+	printf("%x: bit %lu is %s\n", w, bit, (bit_test(w, bit)) ? "ON" : "OFF");
 
-	if (result == 1)           /* Test the status of the n bit */
-		printf("bit %i is ON\n", n);
-	else if (result == 0) {
-		printf("bit %i is OFF\n", n);
-		printf("Turning bit %i ON will make w = %x\n", n, bit_set(w, n));
-	} else
-		printf("Bit is out of word\n");
+	/* change the state of the bit */
+	printf("%x: turned bit %lu ON\n", bit_set(w, bit), bit);
 
+	printf("NOTE: bit 0 is the leftmost bit\n");
 	return 0;
 }
