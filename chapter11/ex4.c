2783ed15ae116242e755eb6a907de5a540a40892
cdude <cdude996@gmail.com>
2019-11-01 14:56:11 -0400

    use revised int_size

--- chapter11/ex4.c
+++ chapter11/ex4.c
@@ -2,46 +2,36 @@
  * 4. Using the result obtained in exercise 3, modify the rotate() function
  * from Program 11.4 so that it no longer makes any assumptions about the size
  * of an int.
+ *
  * By Faisal Saadatmand 
  */
 
 #include <stdio.h>
 
+/* functions */
+size_t int_size();
+unsigned int rotate(unsigned int, int);
+
 /* Function to determine the size of an integer storged on this machine */
-int int_size(int w)
+size_t int_size()
 {
-	int mask, nBits = 0, nibble = 4;
-	
-	mask = ~0;                     /* Integer full of 1's */
-	if (w >= 0) {
-		w = ~w;                    /* One's complement to invert the bits */
-		while (w != mask) {
-			w >>= 1;               /* Right shift until w equals mask */
-			++nBits;
-		}
-		if (nBits % nibble !=0)    /* Ensure each nibble is 4 bits */
-			nBits += nibble - nBits % nibble;
-	} else if (w < 0) {            /* Negative integers */
-		w |= mask;                 /* Set all bits to 1 */
-		while (w != 0) {           /* Left shift until w equal zero */
-			w <<= 1;          
-			++nBits;
-		}
+	int x = ~0;
+	size_t i = 0;
+
+	while (x != 0x0) {
+		x <<= 1;
+		++i;
 	}
 
-	return nBits;
+	return i;
 }
 
 /* Function to rotate an unsigned int left to right */
 unsigned int rotate(unsigned int value, int n)
 {
-	int int_size(int w);
-
 	unsigned int result, bits;
-	int size;
+	const size_t size = int_size();
 
-	size = int_size(value);
-	
 	/* scale down the shift count to a defined range */
 
 	if (n > 0)
@@ -59,14 +49,11 @@ unsigned int rotate(unsigned int value, int n)
 		bits = value << (size - n);
 		result = value >> n | bits;
 	}
-
 	return result;
 }
 
 int main(void) 
 {
-	unsigned int rotate(unsigned int value, int n);
-
 	unsigned int w1 = 0xabcdef00u, w2 = 0xffff1122u;
 
 	printf("%x\n", rotate(w1, 8));
