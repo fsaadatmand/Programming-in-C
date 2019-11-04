636018fe76fa88d66b90db79a507f1c5dbe00e87
cdude <cdude996@gmail.com>
2019-11-04 11:50:53 -0500

    simplify code

--- chapter08/ex3.c
+++ chapter08/ex3.c
@@ -5,60 +5,58 @@
  *
  *		elapsed_time (time1, time2)
  *
- * where time1 represents 3:45:15 and time2
- * represents 9:44:03, should return a time structure that represents 5 hours,
- * 58 minutes, and 48 seconds. Be careful with times that cross midnight.
- * Function to calculate elapsed time.
+ * where time1 represents 3:45:15 and time2 represents 9:44:03, should return a
+ * time structure that represents 5 hours, 58 minutes, and 48 seconds. Be
+ * careful with times that cross midnight.  Function to calculate elapsed time.
+ *
  * By Faisal Saadatmand
  * */
 
 #include <stdio.h>
 
-struct time {
-	int hour;
+struct Time {
+	int hours;
 	int minutes;
 	int seconds;
 };
 
-struct time elapsed_time(struct time t1, struct time t2)
-{
-	int i, j, d;
-	struct time timeDiff = {0, 0 ,0};
-                                       /* convert times into seconds and take their difference */
-	i = t1.hour * 60 * 60 + t1.minutes * 60 + t1.seconds;
-	j = t2.hour * 60 * 60 + t2.minutes * 60 + t2.seconds;
-	d = j - i;
-	
-	if (d < 0)                         /* if time crosses midnight substract 24 hrs */
-		d = d + 86400;
+struct Time elapsed_time(struct Time, struct Time);
 
-	while (d != 0) {                   /* reformat the difference (hh mm ss) */
-		++timeDiff.seconds;
-		if (timeDiff.seconds == 60) {
-			timeDiff.seconds = 0;
-			++timeDiff.minutes;
-			if (timeDiff.minutes == 60) { 
-				timeDiff.minutes = 0;
-				++timeDiff.hour;
-			}
-		}
-		--d;
+struct Time elapsed_time(struct Time t1, struct Time t2)
+{
+	struct Time td; // time difference
+
+	td.seconds = t2.seconds - t1.seconds;
+	td.minutes = t2.minutes - t1.minutes;
+	td.hours = t2.hours - t1.hours;
+
+	if (td.seconds < 0) {
+		td.seconds += 60;
+		--td.minutes;
 	}
-	return timeDiff;
+
+	if (td.minutes < 0) {
+		td.minutes += 60;
+		--td.hours;
+	}
+
+	/* time crosses midnight */
+	if (td.hours < 0)
+		td.hours += 24;
+
+	return td;
 }
 
 int main(void) 
 {
-	struct time elapsed_time(struct time t1, struct time t2);
+	struct Time time_1 = { 3, 45, 15 };
+	struct Time time_2 = { 9, 44, 3 };
+	struct Time time_d;
 
-	struct time time1 = { 3, 45, 15 };
-	struct time time2 = { 9, 44, 3 };
-	struct time elapsedTime;
+	time_d =  elapsed_time(time_1, time_2);
 
-	elapsedTime =  elapsed_time(time1, time2);
-
-	printf("%i hours, %i minutes, %i seconds\n", elapsedTime.hour, elapsedTime.minutes,
-			elapsedTime.seconds);
+	printf("%i hours, %i minutes, %i seconds\n",
+			time_d.hours, time_d.minutes, time_d.seconds);
 
 	return 0;
 }
