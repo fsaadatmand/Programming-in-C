d71e3b898f8989d9b876e6ab53171e616518c8a8
cdude <cdude996@gmail.com>
2019-11-04 13:58:24 -0500

    revised and simplified

--- chapter10/bonus/ex5.6.c
+++ chapter10/bonus/ex5.6.c
@@ -1,43 +1,33 @@
-/* Program to extract and display each typed-in digit of the integer in English:
- * 932 = nine three two 
+/*
+ * Bonus exercise: Program to extract and display each typed-in digit of the
+ * integer in English:
+ *
+ * 		932 -> nine three two
+ *
  * By Faisal Saadatmand
- * */
+  */
 
 #include <stdio.h>
 #include <ctype.h>
 
-struct numInWords {
-	char digit;
-	char numbers[11];
-};
+#define LENGTH 100 
 
 int main(void)
 {
-	int i = 0, j; 
-	char number[30];
-	struct numInWords english_word[30] = 
-	                       { { '0', "zero" }, { '1', "one"}, { '2', "two" },
-	                         { '3', "three" }, { '4', "four" }, { '5', "five" },
-	                         { '6', "six" }, { '7', "seven" }, { '8', "eight" },
-	                         { '9', "nine" } };
-//	char *words[] = { "zero", "one", "two", "three", "four",
-	                          "five", "six", "seven", "eight", "nine" };
+	char number[LENGTH];
+	char *p, *words[] = { "zero", "one", "two", "three", "four",
+					      "five", "six", "seven", "eight", "nine" };
 							  
 	printf("Enter a number: ");
-	scanf("%s", &number);
+	scanf("%99s", number);
 
-	while (number[i] != '\0') {
-		if (isdigit(number[i])) {
-			for (j = 0; j <= 10; ++j)
-				if (number[i] == english_word[j].digit)
-					printf("%s ", english_word[j].numbers);
-		} else {
-			printf("Invalid input\n");
+	for (p = number; p && *p; ++p) {
+		if (!isdigit(*p)) {
+			printf("\nError: %c is not a digit", *p);
 			break;
 		}
-		++i;
+		printf("%s ", words[*p - '0']);
 	}
-
 	printf("\n");
 
 	return 0;
