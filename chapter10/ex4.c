61df88f5728d83411820c78b30166a1d80aa5a9b
cdude <cdude996@gmail.com>
2019-11-04 13:08:26 -0500

    revised code

--- chapter10/ex4.c
+++ chapter10/ex4.c
@@ -12,28 +12,24 @@
 #include <stdio.h>
 
 struct entry {
-	int            value;
-	struct entry   *next;
+	int value;
+	struct entry *next;
 };
 
 void removeEntry(struct entry *element)
 {
-	if (element->next != (struct entry *) 0)
-		element->next = element->next->next;
+	if (!element->next) /* if NULL */
+		element->next = NULL;
 	else
-		printf("End of list... Nothing to remove\n");
+		element->next = element->next->next;
 }
 
 int main(void) 
 {
 	void removeEntry(struct entry *element);
 
-	struct entry n0, n1, n2, n3, n4, n5;
-	struct entry *list_pointer = &n0; /* pointer to the beginning of the list */
-	
-	n0.value = 0;
-	n0.next = &n1;         /* Special structure to remove an element at the 
-							  beginning of the list */
+	struct entry n1, n2, n3, n4, n5; /* list entries */
+	struct entry *list_pointer = &n1; /* pointer to the beginning of the list */
 
 	n1.value = 100;
 	n1.next = &n2;
@@ -48,15 +44,25 @@ int main(void)
 	n4.next = &n5;
 	
 	n5.value = 500;
-	n5.next = (struct entry *) 0; /* Mark list end with null pointer */
+	n5.next =  NULL;
 
+	/* remove entry after n4, i.e. n5 */
+	removeEntry(&n4);
+
+	/* special struct to insert an element at the beginning */
+	struct entry n0 = {0, &n1};
+	/* remove entry after n0, i.e. n1 */
 	removeEntry(&n0);
+	/* update list_pointer to point the new first element, i.e. n2 */
+	list_pointer = n0.next; 
 
-	list_pointer = list_pointer->next;       /* Hide the special structure */
-	while (list_pointer != (struct entry *) 0) {
-		printf("%i ", list_pointer->value);
-		printf("%p\n", list_pointer->next);
+	int i = 1;
+	while (list_pointer != NULL) {
+		printf("n%i (%p)\n", i++, list_pointer);
+		printf(" value: %i\n", list_pointer->value);
+		printf(" next: %p\n", list_pointer->next);
 		list_pointer = list_pointer->next;
 	}
+
 	return 0;
 }
