47ecd3951d3de0f17308bbdc1da59472a38fa979
cdude <cdude996@gmail.com>
2019-11-04 13:06:07 -0500

    simplify code

--- chapter10/ex8.c
+++ chapter10/ex8.c
@@ -7,33 +7,36 @@
 
 #include <stdio.h>
 
-void sort3(int *a, int *b, int *c)
-{	
+/* functions */
+void swap(int *, int *);
+void sort3(int *, int *, int *);
+
+/* swap: swap the objects pointed to by p with the object pointed to by q. */
+void swap(int *p, int *q)
+{
 	int temp;
 
-	if (*a > *b) {
-		temp = *b;
-		*b = *a;
-		*a = temp;
-	}
-	if (*a > *c) {
-		temp = *c;
-		*c = *a;
-		*a = temp;
-	}
-	if (*b > *c) {
-		temp = *c;
-		*c = *b;
-		*b = temp;
-	}
+	temp = *p;
+	*p = *q;
+	*q = temp;
+}
+
+/* sort3: sorts 3 integers in ascending order (uses bubble sort algorithm) */
+void sort3(int *a, int *b, int *c)
+{	
+	if (*a > *b)
+		swap(a, b);
+	if (*a > *c)
+		swap(a, c);
+	if (*b > *c)
+		swap(b, c);
 }
 
 int main(void) 
 {
-	void sort3(int *a, int *b, int *c);
-
 	int i1 = 110, i2 = 345, i3 = 81;
 
+	printf("%i %i %i -> ", i1, i2, i3); 
 	sort3(&i1, &i2, &i3);
 	printf("%i %i %i\n", i1, i2, i3); 
 
