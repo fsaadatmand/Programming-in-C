b8b86a2ac328c9d7cb8f27cb6bf5c5df7d584a8f
cdude <cdude996@gmail.com>
2019-11-04 00:45:06 -0500

    simplify code `

--- chapter09/ex3.c
+++ chapter09/ex3.c
@@ -10,54 +10,46 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-/* Function to determine if a character is alphabetic */
-bool alphabetic(const char c)
+/* functions */
+bool alphaNumeric(const char);
+int countWords(const char []);
+
+/* alphaNumeric: returns true if c is an alphanumeric character,
+ * otherwise, return false */
+bool alphaNumeric(const char c)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') 
+			|| (c >= '0' && c <= '9'))
 		return true;
-	else
-		return false;
+	return false;
 }
 
-/* function to determine if a character is a number */
-bool numeric(const char c)
+/* specialChar: returns true if c is a special character as defined in the
+ * function, otherwise return false */
+bool specialChar(const char c)
 {
-	if (c >= '0' && c <='9')
+	if (c == '\'' || c == '-' || c == '+' || c == ',' || c  == '.')
 		return true;
-	else
-		return false;
+	return false;
 }
 
 /* Function to count the number of words in a string */
 int countWords(const char string[])
 {
-	bool alphabetic(const char c), numeric(const char c); 
-
 	int  i, wordCount = 0;
-	bool lookingForWord = true, isLetter = false, isNumber = false; 
+	bool lookingForWord = true;
 
-	for (i = 0; string[i] != '\0'; ++i) 
-		if (alphabetic(string[i])) {
+	for (i = 0; string[i] != '\0'; ++i) {
+		if (specialChar(string[i]))
+			continue; // skip special charachters
+		if (alphaNumeric(string[i])) {
 			if (lookingForWord) {
 				++wordCount;
 				lookingForWord = false;
-				isLetter = true;
 			}
-		} else if (numeric(string[i])) {
-			if (lookingForWord) {
-				++wordCount;
-				lookingForWord = false;
-				isNumber = true; 
-			}
-		/* words with an postraphe */
-		} else if (string[i] == '\'' && isLetter)
-			lookingForWord = false;
-		/* number with an embeded comma or period */
-		else if ((string[i] == 44 || string[i] == 46) && isNumber)
-			lookingForWord = false;
-		else
+		} else
 			lookingForWord = true;
-
+	}
 	return wordCount;
 }
 
@@ -67,13 +59,13 @@ int main(void)
 	const char text2[] = "And here we go...again.";
 	const char text3[] = "We've done it.";
 	const char text4[] = "let's try some numbers 123,45 55 9.4.";
+	const char text5[] = "-123,45 55 +9.4";
 
-	int   countWords(const char string[]);
-
-	printf("%s - words = %i\n", text1, countWords(text1));
-	printf("%s - words = %i\n", text2, countWords(text2));
-	printf("%s - words = %i\n", text3, countWords(text3));
-	printf("%s - words = %i\n", text4, countWords(text4));
+	printf("%s -> %i\n", text1, countWords(text1));
+	printf("%s -> %i\n", text2, countWords(text2));
+	printf("%s -> %i\n", text3, countWords(text3));
+	printf("%s -> %i\n", text4, countWords(text4));
+	printf("%s -> %i\n", text5, countWords(text5));
 
 	return 0;
 }
