9c5a375675740763c761d8e4ad558c7a72e22035
cdude <cdude996@gmail.com>
2019-11-04 13:04:52 -0500

    cleanup

--- chapter10/ex11.c
+++ chapter10/ex11.c
@@ -16,49 +16,50 @@ struct date {
 	int year;
 };
 
+/* functions */
+void dateUpdate(struct date *);
+int numberOfDays(struct date *);
+bool isLeapYear(struct date *);
+
 /* Function to calculate tomorrow's date */
 void dateUpdate(struct date *today)
 {
-	int numberOfDays(struct date d);
-	struct date todayDate = *today;
-
-	if (today->day != numberOfDays(todayDate)) {
+	if (today->day != numberOfDays(today)) {
 		today->day += 1;
-	} else if (today->month == 12) {                    /* end of year */
+	} else if (today->month == 12) { /* end of year */
 		today->day = 1;
 		today->month = 1;
 		today->year += 1;
-	} else {                                            /* end of month */
+	} else { /* end of month */
 		today->day = 1;
 		today->month += 1;
 	}
 }
 
 /* Function to find the number of days in a month */
-int numberOfDays(struct date d)
+int numberOfDays(struct date *d)
 {
 	int days;
-	bool isLeapYear(struct date d);
 	const int daysPerMonth[12] = { 31, 28, 31, 30, 31, 30,
 		                           31, 31, 30, 31, 30, 31 };
 
-	if (isLeapYear(d) == true && d.month == 2)
+	if (isLeapYear(d) == true && d->month == 2)
 		days = 29;
 	else
-		days = daysPerMonth[d.month - 1];
+		days = daysPerMonth[d->month - 1];
 
 	return days;
 }
 
 /* Function to determine if it's a leap year */
-bool isLeapYear(struct date d)
+bool isLeapYear(struct date *d)
 {
 	bool leapYearFlag;
 
-	if ((d.year % 4 == 0 && d.year % 100 != 0) || d.year % 400 == 0)
-		leapYearFlag = true;    /* it's a leap year */
+	if ((d->year % 4 == 0 && d->year % 100 != 0) || d->year % 400 == 0)
+		leapYearFlag = true; /* it's a leap year */
 	else
-		leapYearFlag = false;    /* Not a leap year */
+		leapYearFlag = false; /* Not a leap year */
 	
 	return leapYearFlag;
 }
@@ -67,11 +68,8 @@ int main(void)
 {
 	struct date thisDay;
 
-	void dateUpdate(struct date *today);
-	bool isLeapYear(struct date d);
-
 	printf("Enter today's date (dd mm yyyy): ");
-	scanf("%i%i%i", &thisDay.day, &thisDay.month, &thisDay.year);
+	scanf("%d%d%d", &thisDay.day, &thisDay.month, &thisDay.year);
 
 	dateUpdate(&thisDay);
 		
