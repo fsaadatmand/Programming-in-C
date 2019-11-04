e0cba87487635a5b51945b9ae2713ea19ec5b33e
cdude <cdude996@gmail.com>
2019-11-04 12:55:56 -0500

    minor tweaks

--- chapter08/ex6.c
+++ chapter08/ex6.c
@@ -2,40 +2,46 @@
  * 6. Replace the dateUpdate() function from Program 8.4 with the modified one
  * that uses compound literals as presented in the text. Run the program to
  * verify its proper operation.
+ *
  * By Faisal Saadatmand
  * */
 
 #include <stdio.h>
 #include <stdbool.h>
 
-struct date {
+struct Date {
 	int day;
 	int month;
 	int year;
 };
 
-/* Function to calculate tomorrow's date */
-struct date dateUpdate(struct date today)
-{
-	struct date tomorrow;
+/* functions */
+struct Date dateUpdate(struct Date);
+int numberOfDays(struct Date);
+bool isLeapYear(struct Date);
 
-	int numberOfDays(struct date d);
+/* Function to calculate tomorrow's date */
+struct Date dateUpdate(struct Date today)
+{
+	struct Date tomorrow;
+
+	int numberOfDays(struct Date d);
 
 	if (today.day != numberOfDays (today)) {
-		tomorrow = (struct date) {today.day + 1, today.month, today.year };
-	} else if (today.month == 12) {                           /* end of year */
-		tomorrow = (struct date) {1, 1, today.year + 1};
-	} else {                                                  /* end of month */
-		tomorrow = (struct date) {1, today.month + 1, today.year};
+		tomorrow = (struct Date) {today.day + 1, today.month, today.year };
+	} else if (today.month == 12) { /* end of year */
+		tomorrow = (struct Date) {1, 1, today.year + 1};
+	} else { /* end of month */
+		tomorrow = (struct Date) {1, today.month + 1, today.year};
 	}
 	return tomorrow;
 }
 
 /* Function to find the number of days in a month */
-int numberOfDays(struct date d)
+int numberOfDays(struct Date d)
 {
 	int days;
-	bool isLeapYear(struct date d);
+	bool isLeapYear(struct Date d);
 	const int daysPerMonth[12] = { 31, 28, 31, 30, 31, 30,
 		                           31, 31, 30, 31, 30, 31 };
 
@@ -47,7 +53,7 @@ int numberOfDays(struct date d)
 }
 
 /* Function to determine if it's a leap year */
-bool isLeapYear(struct date d)
+bool isLeapYear(struct Date d)
 {
 	bool leapYearFlag;
 
@@ -60,17 +66,14 @@ bool isLeapYear(struct date d)
 
 int main(void)
 {
-	struct date thisDay, nextDay;
-
-	struct date dateUpdate(struct date today);
-	bool isLeapYear(struct date d);
+	struct Date thisDay, nextDay;
 
 	printf("Enter today's date (dd mm yyyy): ");
-	scanf("%i%i%i", &thisDay.day, &thisDay.month, &thisDay.year);
+	scanf("%d%d%d", &thisDay.day, &thisDay.month, &thisDay.year);
 
 	nextDay = dateUpdate(thisDay);
 
-	printf("Tomorrow date is %i/%i/%.2i.\n", nextDay.day, nextDay.month,
+	printf("Tomorrow Date is %d/%d/%.2d.\n", nextDay.day, nextDay.month,
 			nextDay.year % 100);
 
 	return 0;
