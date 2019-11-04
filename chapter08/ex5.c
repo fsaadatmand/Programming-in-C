8c1d654697be5baa88d5133cec9a8fae6230d3a8
cdude <cdude996@gmail.com>
2019-11-04 12:24:38 -0500

    minor tweaks

--- chapter08/ex5.c
+++ chapter08/ex5.c
@@ -4,31 +4,39 @@
  * the timeUpdate() function, and if the time reaches midnight, the function
  * should call the dateUpdate function to switch over to the next day. Have the
  * function return the updated dateAndTime structure.
+ *
  * By Faisal Saadatmand
  */
 
 #include <stdio.h>
 #include <stdbool.h>
 
-struct date {
+struct Date {
 	int month;
 	int day;
 	int year;
 };
 
-struct time {
+struct Time {
 	int hour;
 	int minutes;
 	int seconds;
 };
 
-struct dateAndTime {
-	struct date sdate;
-	struct time stime;
+struct DateAndTime {
+	struct Date sdate;
+	struct Time stime;
 };
 
+/* functions */
+struct Time timeUpdate(struct Time);
+bool isLeapYear(struct Date);
+int numberOfDays(struct Date);
+struct Date dateUpdate(struct Date);
+struct DateAndTime clockKeeper(struct DateAndTime);
+
 /* function to update the time by one second */
-struct time timeUpdate(struct time now)
+struct Time timeUpdate(struct Time now)
 {
 	++now.seconds;
 	if (now.seconds == 60) {         /* next minute */
@@ -45,7 +53,7 @@ struct time timeUpdate(struct time now)
 }
 
 /* Function to determine if it's a leap year */
-bool isLeapYear(struct date d)
+bool isLeapYear(struct Date d)
 {
 	bool leapYearFlag;
 
@@ -57,13 +65,13 @@ bool isLeapYear(struct date d)
 }
 
 /* function to find the number of days in a month */
-int numberOfDays(struct date d)
+int numberOfDays(struct Date d)
 {
 	int days;
-	bool isLeapYear(struct date d);
+	bool isLeapYear(struct Date d);
 
-	const int daysPerMonth[12] =
-	{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	const int daysPerMonth[12] = {31, 28, 31, 30, 31, 30, 31, 31,
+								  30, 31, 30, 31};
 
 	if (isLeapYear(d) && d.month == 2)
 		days = 29;
@@ -73,40 +81,36 @@ int numberOfDays(struct date d)
 }
 
 /* function to update the date by one day */
-struct date dateUpdate(struct date today)
+struct Date dateUpdate(struct Date today)
 {
-	struct date tomorrow;
-	int numberOfDays(struct date d);
+	struct Date tomorrow;
+	int numberOfDays(struct Date d);
 
-	if (today.day != numberOfDays (today))
-		tomorrow = (struct date) {today.month, today.day + 1, today.year};
+	if (today.day != numberOfDays(today))
+		tomorrow = (struct Date) {today.month, today.day + 1, today.year};
 	else if (today.month == 12)                       /* end of year */
-		tomorrow = (struct date) {1, 1, today.year + 1};
+		tomorrow = (struct Date) {1, 1, today.year + 1};
 	else                                              /* end of month */
-		tomorrow = (struct date) {today.month + 1, 1, today.year};
+		tomorrow = (struct Date) {today.month + 1, 1, today.year};
 	return tomorrow;
 }
 
 
-struct dateAndTime clockKeeper(struct dateAndTime d)
+struct DateAndTime clockKeeper(struct DateAndTime d)
 {
 	d.stime = timeUpdate(d.stime);	
-	if (d.stime.hour == 0)                           /* change date on midnight */
+	if (d.stime.hour == 0) /* change date on midnight */
 		d.sdate = dateUpdate(d.sdate);
 	return d;
 }
 
 int main(void)
 {
-	struct time           timeUpdate(struct time now);
-    bool                  isLeapYear(struct date d);
-    int                   numberOfDays(struct date d);
-    struct date           dateUpdate(struct date today);
-	struct dateAndTime    clockKeeper(struct dateAndTime d);
-	struct dateAndTime    event = 
-	                      { { 12, 31, 2018 }, { 23, 59, 59 } };
+	struct DateAndTime event = { { 12, 31, 2018 }, { 23, 59, 59 } };
+
 	event = clockKeeper(event);
-	printf("%i/%i/%i, %.2i:%.2i:%.2i\n", event.sdate.day, event.sdate.month,
+
+	printf("%i/%i/%i %.2d:%.2d:%.2d\n", event.sdate.day, event.sdate.month,
 			event.sdate.year, event.stime.hour, event.stime.minutes, 
 			event.stime.seconds);
 
