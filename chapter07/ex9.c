9bd1b470b14800f24414fa017dfd4275c24f241f
cdude <cdude996@gmail.com>
2019-11-05 13:14:25 -0500

    refine answer

--- chapter07/ex9.c
+++ chapter07/ex9.c
@@ -5,49 +5,63 @@
  * integer divisible by both 15 and 10. Write a function lcm() function should
  * calculate the least common multiple by calling the gcd() function from
  * Program 7.6 in accordance with the following identity:
+ *
  *		lcm (u, v) = uv / gcd (u, v)        u, v >= 0
+ *
  * By Faisal Saadatmand
  */
 
 #include <stdio.h>
 
+/* functions */
+int absoluteValue(int);
+int gcd(int, int);
+int lcm(int, int);
+
+int absoluteValue(int x)
+{
+	if (x < 0)
+		x = -x;
+
+	return x;
+}
+
+/* gcd: find and return the greatest common divisor of two non-negative
+ * integers vales. */
 int gcd(int u, int v)
 {
 	int temp;
 
-	while (v !=0) {
+	while (v != 0) {
 		temp = u % v;
 		u = v;
 		v = temp;
 	}
+
 	return u;
 }
 
+/* lcm: calculate and return the least common multiplier of two non-negative
+ * integers */
 int lcm(int u, int v)
 {
-	if (u < 0 || v < 0)
-		return 1;
-	else
-		return (u * v) / gcd(u, v);
+	if (u == 0 && v == 0)
+		return 0;
+
+	/* ignore negative signs, if any */
+	u = absoluteValue(u);
+	v = absoluteValue(v);
+
+	return (u * v) / gcd(u, v);
 }
 
 int main(void) 
 {
 	int u, v;
 
-	int gcd(int u, int v);
-	int lcm(int u, int v);
-
-	printf("Enter value for u: ");
-	scanf("%i", &u);
-
-	printf("Enter value for v: ");
-	scanf("%i", &v);
-
-	if (lcm(u, v) == 1)
-		printf("The value of u and v must be a positive integers\n");
-	else
-		printf("The lcm of %i and %i is %i\n", u, v, lcm(u, v));
+	printf("Enter two numbers to calculate the least common multiplier:\n");
+	scanf("%i %i", &u, &v);
+	printf("The LCM of %i and %i is %i\n", u, v, lcm(u, v));
 
 	return 0;
 }
