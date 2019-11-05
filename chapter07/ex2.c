a11dfc715f1e2a0e128521a9864f06e483356e59
cdude <cdude996@gmail.com>
2019-11-05 09:57:51 -0500

    fix answer

--- chapter07/ex2.c
+++ chapter07/ex2.c
@@ -2,10 +2,14 @@
  * 2. Modify Program 7.4 so the value of triangularNumber is returned by the
  * function. Then go back to Program 4.5 and change that program so that it
  * calls the new version of the calculateTriangularNumber() function.
+ *
  * By Faisal Saadatmand
  */
 
 #include <stdio.h>
+
+/* functions */
+int calculateTriangularNumber(int);
 
 int calculateTriangularNumber(int n)
 {
@@ -20,14 +24,14 @@ int calculateTriangularNumber(int n)
 
 int main(void)
 {
-    int number;
-	int calculateTriangularNumber(int n);
+    int number, counter;
 
-    printf("Enter your number.\n");
-    scanf("%i", &number);
-
-	printf("Triangular number for %i is %i\n", number,
-			calculateTriangularNumber(number));
+	for (counter = 0; counter <= 5; ++counter) {
+		printf("What triangular number do you want? ");
+		scanf("%i", &number);
+		printf("Triangular number %i is %i\n", number,
+				calculateTriangularNumber(number));
+	}
 
     return 0;
 }
