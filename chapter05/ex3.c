0d1bc390e02d35b6449831397e7d1159f714be0c
cdude <cdude996@gmail.com>
2019-11-04 21:50:39 -0500

    minor tweaks

--- chapter05/ex3.c
+++ chapter05/ex3.c
@@ -3,6 +3,7 @@
  * Display the result of dividing the first integer by the second, to three-
  * decimal-place accuracy. Remember to have the program check for division by
  * zero.
+ *
  * By Faisal Saadatmand
  */
 
@@ -13,13 +14,15 @@ int main(void)
 	int number1, number2;
 	float result;
 
-	printf("Type in two digits:\n");
-	scanf("%i %i", &number1, &number2);
+	printf("Type in two integers:\n");
+	scanf("%d %d", &number1, &number2);
 
-	if (number2 == 0)
-		printf("Division by zerp");
-	else
-		result = (float) number1 / number2;
+	if (number2 == 0) {
+		printf("Division by zero");
+		return -1;
+	}
+
+	result = (float) number1 / number2;
 
 	printf("%i divided by %i is %.3f\n", number1, number2, result); 
 
