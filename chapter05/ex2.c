05a51a4115cbc0c591c6822a290556690367d5a9
cdude <cdude996@gmail.com>
2019-11-04 21:50:01 -0500

    minor tweaks

--- chapter05/ex2.c
+++ chapter05/ex2.c
@@ -3,6 +3,7 @@
  * terminal. Test these two numbers to determine if the first is evenly
  * divisible by the second, and then display an appropriate message at the
  * terminal.
+ *
  * By Faisal Saadatmand
  */
 
@@ -16,9 +17,9 @@ int main(void)
 	scanf("%i %i", &number1, &number2);
 
 	if (number1 % number2 == 0)
-		printf("%i is evely divisable by %i\n", number1, number2);
-	else if (number1 % number2 != 0)
-		printf("%i is NOT evenly divisable by %i\n", number1, number2);
+		printf("%i is evenly divisible by %i\n", number1, number2);
+	else
+		printf("%i is NOT evenly divisible by %i\n", number1, number2);
 
 	return 0;
 }
