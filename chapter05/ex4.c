68b016cd937568c63efb90d6594d1ee195a6b413
cdude <cdude996@gmail.com>
2019-11-04 21:48:20 -0500

    use switch instead of if-else statements

--- chapter05/ex4.c
+++ chapter05/ex4.c
@@ -1,33 +1,39 @@
 /*
  * 4. Write a program that acts as a simple “printing” calculator. The program
  * should allow the user to type in expressions of the form
+ * 
  *		number   operator
+ *
  * The following operators should be recognized by the program:
+ *
  *		+     -     *     /     S     E
+ *
  * The S operator tells the program to set the “accumulator” to the typed-in
  * number. The E operator tells the program that execution is to end. The
  * arithmetic operations are performed on the contents of the accumulator with
  * the number that was keyed in acting as the second operand. The following is
  * a “sample run” showing how the program should operate:
  *
- * Begin Calculations
- * 10 S           Set Accumulator to 10
- * = 10.000000    Contents of Accumulator
- * 2 /            Divide by 2
- * = 5.000000     Contents of Accumulator
- * 55 -           Subtract 55
- * -50.000000
- *  100.25 S       Set Accumulator to 100.25
- * = 100.250000
- * 4 *            Multiply by 4
- * = 401.000000
- * 0 E            End of program
- * = 401.000000
- * End of Calculations.
+ *		Begin Calculations
+ *		10 S           Set Accumulator to 10
+ *		= 10.000000    Contents of Accumulator
+ *		2 /            Divide by 2
+ *		= 5.000000     Contents of Accumulator
+ *		55 -           Subtract 55
+ *		-50.000000
+ *		100.25 S       Set Accumulator to 100.25
+ *		= 100.250000
+ *		4 *            Multiply by 4
+ *		= 401.000000
+ *		0 E            End of program
+ *		= 401.000000
+ *		End of Calculations.
  *
  * Make certain that the program detects division by zero and also checks for
  * unknown operators.
- * By Faisal Saadatmand */
+ *
+ * By Faisal Saadatmand
+ */
 
 #include <stdio.h>
 
@@ -39,23 +45,31 @@ int main(void)
 	printf("Begin Calculations\n\n");
 	scanf("%f %c", &number, &operator);
 
+	accumulator = 0;
 	while (operator != 'E') {
-		if (operator == 'S')
+		switch (operator) {
+		case 'S':
 			printf("= %f\n\n", accumulator = number);
-		else if (operator == '+')
+			break;
+		case '+':
 			printf("= %f\n\n", accumulator += number);
-		else if (operator == '-')
+			break;
+		case '-':
 			printf("= %f\n\n", accumulator -= number);
-		else if(operator == '*')
+			break;
+		case '*':
 			printf("= %f\n\n", accumulator *= number);
-		else if (operator == '/') {
+			break;
+		case '/':
 			if (number == 0)
 				printf("Division by zero\n\n");
 			else
 				printf("= %f\n\n", accumulator /= number);
-		} else
+			break;
+		default:
 			printf("Uknown operator\n\n");
-
+			break;
+		}
 		scanf("%f %c", &number, &operator);
 	}
 	
