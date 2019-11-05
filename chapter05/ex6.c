b2ae6c73444479b555f67ca4dd12042b0baca2be
cdude <cdude996@gmail.com>
2019-11-04 21:49:14 -0500

    revised answer

--- chapter05/ex6.c
+++ chapter05/ex6.c
@@ -2,11 +2,12 @@
  * 6. Write a program that takes an integer keyed in from the terminal and
  * extracts and displays each digit of the integer in English. So, if the user
  * types in 932, the program should display
- *		nine three two Remember to display
- * “zero” if the user types in just a 0. (Note: This exercise is a hard one!)
  *
- * Attempt #1: while this code works, I wrote a slightly better version that can
- * also handle numbers ending with zeros. See ex6a.c
+ *		nine three two
+ *
+ * Remember to display “zero” if the user types in just a 0. (Note: This
+ * exercise is a hard one!)
+ *
  * By Faisal Saadatmand
  */
 
@@ -14,67 +15,73 @@
 
 int main(void)
 {
-	int number, number_temp, number_of_digits, right_number, multiplier,
-		reversed_number, n;
+	int number, reversed_number, right_digit, trailing_zeros;
 
 	printf("Enter a number: ");
 	scanf("%d", &number);
 
-	number_temp = number;
-	multiplier = 0;
-	number_of_digits = 0;
-	reversed_number = 0;
+	/* If number is 0, simply print zero */
+	if (number == 0)
+		printf("zero");
 
-/* The role of the initial loop is twofold: 
- * 1. To count the number of digits in the typed-in number; 
- * 2. Calculate a  multiplier variable based on the number of digits. */ 
-	do {
-		right_number = number_temp % 10;
-		number_temp /= 10;
-		++number_of_digits;
-
-		if (multiplier == 0)
-			multiplier = 1;
-		else
-			multiplier *= 10;
-	} while (number_temp != 0);
-
-/* This loop uses the multiplier from the previous loop to store the number in reverse in  variable "reversed_number".  */
-	for (n = 1; n <= number_of_digits; ++n) {
-		right_number = number % 10;
-		right_number *= multiplier ;
-		reversed_number += right_number;
+	/* extract and count trailing 0's if any */
+	trailing_zeros = 0;
+	while (number != 0 && (number % 10) == 0) {
+		++trailing_zeros;
 		number /= 10;
-		multiplier /= 10;
 	}
 
-/* final loop to reads and prints the each digit from the reversed number--from right to left--and converts it to the equivalent English words, which will give us the desired result */
-	do {
-		right_number = reversed_number % 10;
+	/* reverse the remaining integer */
+	reversed_number = 0;
+	while (number > 0) {
+		right_digit = number % 10;
+		reversed_number = reversed_number * 10 + right_digit;
+		number /= 10;
+	}
 
-		if (right_number == 0 )
+	/* print reversed integer from right to left in english words */
+	while (reversed_number > 0) {
+		right_digit = reversed_number % 10;
+		switch (right_digit) {
+		case 0:
 			printf("zero ");
-		else if (right_number == 1)
+			break;
+		case 1:
 			printf("one ");
-		else if (right_number == 2)
+			break;
+		case 2:
 			printf("two ");
-		else if (right_number == 3)
+			break;
+		case 3:
 			printf("three ");
-		else if (right_number == 4)
+			break;
+		case 4:
 			printf("four ");
-		else if (right_number == 5)
+			break;
+		case 5:
 			printf("five ");
-		else if (right_number == 6)
+			break;
+		case 6:
 			printf("six ");
-		else if (right_number == 7)
+			break;
+		case 7:
 			printf("seven ");
-		else if (right_number == 8)
+			break;
+		case 8:
 			printf("eight ");
-		else if (right_number == 9)
+			break;
+		case 9:
 			printf("nine ");
+			break;
+		default:
+			break;
+		}
 		reversed_number /= 10;
-		} while (reversed_number != 0);
-
+	}
+	/* print trailing 0's if any */
+	while (trailing_zeros--) {
+		printf("zero ");
+	}
 	printf("\n");
 
 	return 0;
