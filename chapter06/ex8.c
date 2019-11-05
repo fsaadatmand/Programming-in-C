dca0362371e083fbd21177c3d1d56b5e36141351
cdude <cdude996@gmail.com>
2019-11-05 09:49:54 -0500

    use a simpler example

--- chapter06/ex8.c
+++ chapter06/ex8.c
@@ -2,10 +2,6 @@
  * 8. Find out if your compiler supports variable-length arrays. If it does,
  * write a small program to test the feature out.
  *
- * This a modified version of ex6. This version uses an array (Prime) to store
- * the result and a variable length array to produce a clean output (without
- * zeroes) for printf. See steps 7 and 8.
- *
  * By Faisal Saadatmand
  */
 
@@ -13,73 +9,22 @@
 
 int main(void)
 {
+	unsigned long n, i;
 
-/* Step 1: define an array of integers P. Set all elements to 
- * P[i] to 0, 2 <= i <= n */
+	printf("Enter a value of n to Generate the first n Fibonacci numbers: ");
+	scanf("%lu", &n);
 
-	int  P[151], N[151], Prime[151], i, j, n, x, counter; 
+	/* array's depends on the user's input */
+	unsigned long long Fibonacci[n];
 
-	n = 150;
-	counter = 0;
+	Fibonacci[0] = 0;
+	Fibonacci[1] = 1;
 
+	for (i = 2; i < n; ++i)
+		Fibonacci[i] = Fibonacci[i - 2] + Fibonacci [i - 1];
 
-	for (i = 2; i <= n; ++i) {
-		P[i] = 0;
-		N[i] = i;
-		Prime[i] = 0;
-	}
-
-/* Step 2: set i to 2 */
-	i = 2;
-
-/* Step 3: If i > n, the algorithm terminates */
-	while (i < n) {
-
-/* Step 4: If P[i] is 0, then i is prime */
-		if (P[i] == 0) 
-			Prime[i] = N[i];
-
-/* Step 5: For all positive integer values of j, such
- * i x j <= n, set P[i x j] to 1. */
-
-		for (j = 2; j <= n; ++j) {	
-			if (i * j <= n)
-				P[i*j] = 1;
-		}
-	
-/* Step 6: Add 1 to i and go to step 3 */
-	++i;
-	}
-
-/* Step 7: Order the Prime array */
-	for (i = 2; i <= n; ++i)
-		if (Prime[i] == 0) 
-				++counter;
-		else if (Prime[i] != 0 && counter != 0) {
-			for ( x = i; x <= n; ++x )
-				Prime[x-counter] = Prime[x];
-
-			i -= counter;
-			counter = 0;
-		}
-
-/* Step 8: Create a variable length array Prime2 to eliminate the zeroes at the end of Prime array. */
-	x = 2;
-
-	while ( Prime[x] != 0 )
-		++x;
-
-	unsigned int Prime2[x];
-
-	for (i = 2; i <= x; ++i)
-		Prime2[i]  = Prime[i];
-
-/* Print the value of elements in Prime2 */
-
-	for (i = 2; i < x ; ++i)
-		printf("%i ", Prime2[i]);
-	
-	printf("\n");
+	for (i = 0; i < n; ++i)
+		printf("%llu\n", Fibonacci[i]);
 
 	return 0;
 }
