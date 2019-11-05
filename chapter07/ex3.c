23a25f96520913c3339e39b529cef9b8a6c13d03
cdude <cdude996@gmail.com>
2019-11-05 10:01:44 -0500

    minor tweaks

--- chapter07/ex3.c
+++ chapter07/ex3.c
@@ -8,12 +8,17 @@
 
 #include <stdio.h>
 
+/* functions */
+float absoluteValue(float);
+float squareRoot(float, const float);
+
 /* Function to calculate the absolute value of a number */
 float absoluteValue(float x)
 {
 	if (x < 0)
 		x = -x;
-	return (x);
+
+	return x;
 }
 
 /* Function to compute the square root of a number */
@@ -23,14 +28,17 @@ float squareRoot(float x, const float epsilon)
 
 	while (absoluteValue(guess * guess - x) >= epsilon)
 		guess = (x / guess + guess) / 2.0;
+
 	return guess;
 }
 
 int main(void) 
 {
-	printf("squareRoot(2.0) = %f\n", squareRoot(2.0, .00001));
-	printf("squareRoot(144.0) = %f\n", squareRoot(144.0, .00001));
-	printf("squareRoot(17.5) = %f\n", squareRoot(17.5, .00001));
+	const float epsilon = .00001;
+
+	printf("squareRoot(2.0) = %f\n", squareRoot(2.0, epsilon));
+	printf("squareRoot(144.0) = %f\n", squareRoot(144.0, epsilon));
+	printf("squareRoot(17.5) = %f\n", squareRoot(17.5, epsilon));
 
 	return 0;
 }
