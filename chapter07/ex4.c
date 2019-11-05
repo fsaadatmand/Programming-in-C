ca8a838f63c1b63668dae3cc5b45e7b1e5de634e
cdude <cdude996@gmail.com>
2019-11-05 10:10:18 -0500

    add answer and perform some minor tweaks

--- chapter07/ex4.c
+++ chapter07/ex4.c
@@ -4,7 +4,14 @@
  * the square root. What conclusions can you reach about the number of
  * iterations through the loop, the number whose square root is being
  * calculated, and the value of the initial guess?
+ *
  * By Faisal Saadatmand
+ */
+
+/*
+ * Answer: The greater the number whose square root is being take, the greater
+ * the initial guess, and thus, the more iteration through loop it would take
+ * to approach the value of epsilon.
  */
 
 #include <stdio.h>
@@ -14,26 +21,29 @@ float absoluteValue(float x)
 {
 	if (x < 0)
 		x = -x;
-	return (x);
+
+	return x;
 }
 
 /* Function to compute the square root of a number */
 float squareRoot(float x, const float epsilon)
 {
-	float guess         = 1.0;
+	float guess = 1.0;
 
 	while (absoluteValue(guess * guess - x) >= epsilon) {
 		guess = (x / guess + guess) / 2.0;
 		printf("%f\n", guess);
 	}
+
 	return guess;
 }
 
 int main (void) 
 {
-	printf("squareRoot(2.0) = %f\n", squareRoot(2.0, .00001));
-	printf("squareRoot(144.0) = %f\n", squareRoot(144.0, .00001));
-	printf("squareRoot(17.5) = %f\n", squareRoot(17.5, .00001));
+	const float epsilon = .00001;
+	printf("squareRoot(2.0) = %f\n", squareRoot(2.0, epsilon));
+	printf("squareRoot(144.0) = %f\n", squareRoot(144.0, epsilon));
+	printf("squareRoot(17.5) = %f\n", squareRoot(17.5, epsilon));
 
 	return 0;
 }
