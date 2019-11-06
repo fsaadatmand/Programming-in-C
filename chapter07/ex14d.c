69ad7b1cc307e9e2c8de4e186a964aa58524c054
cdude <cdude996@gmail.com>
2019-11-06 10:12:11 -0500

    revised answer

--- chapter07/ex14d.c
+++ chapter07/ex14d.c
@@ -7,57 +7,51 @@
  */
 
 #include <stdio.h>
+#include <stdbool.h>
 	
-int array[16] = { 34, -5, 6, 0, 12, 100, 56, 22,
-                 44, -3, -9, 12, 17, 22, 6, 11 };
+/* globals */
+bool gSortOrder;
 
-void sort(int a[], int n, char s)
+/* functions */
+void sort(int [], int);
+
+void sort(int a[], int n)
 {
-	int i, j, temp;
+	int i, j, temp, cond;
 
-	if (s == 'a') {
-		for (i = 0; i < n - 1; ++i)
-			for (j = i + 1; j < n; ++j)
-				if (a[i] > a[j]) {
-					temp = a[i];
-					a[i] = a[j];
-					a[j] = temp;
-				}
-	} else if (s == 'd') {
-		for (i = 0; i < n - 1; ++i)
-			for (j = i + 1; j < n; ++j)
-				if (a[i] < a[j]) {
-					temp = a[i];
-					a[i] = a[j];
-					a[j] = temp;
-				}
-	} else
-		printf("\n\n %c is an invalid sort criteria\n", s);
+	for (i = 0; i < n - 1; ++i) 
+		for (j = i + 1; j < n; ++j) {
+			cond = (gSortOrder) ? a[i] < a[j] : a[i] > a[j];
+			if (cond) {
+				temp = a[i];
+				a[i] = a[j];
+				a[j] = temp;
+			}
+		}
 }
 
 int main(void) 
 {
 	int i;
+	int array[16] = { 34, -5, 6, 0, 12, 100, 56, 22,
+                  44, -3, -9, 12, 17, 22, 6, 11 };
 
-	void sort(int a[], int n, char s);
-
-	printf("The array before the sort:\n");
+	printf("\nBefore sorting:\n");
 	for (i = 0; i < 16; ++i)
 		printf(" %i", array[i]);
 
-	sort(array, 16, 'a');
-
-	printf("\n\nThe array after the sort in ascending order:\n");
+	gSortOrder = false; /* ascending order */
+	printf("\n\nSorted in ascending order:\n");
 	for (i = 0; i < 16; ++i)
 		printf(" %i", array[i]);
 
-	sort(array, 16, 'd');
-
-	printf("\n\nThe array after the sort in descending order:\n");
+	gSortOrder = true; /* descending order */
+	sort(array, 16);
+	printf("\n\nSorted in descending order:\n");
 	for (i = 0; i < 16; ++i)
 		printf(" %i", array[i]);
 
-	printf("\n");
+	printf("\n\n");
 
 	return 0;
 }
