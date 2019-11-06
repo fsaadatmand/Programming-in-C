688fc9c7317fb996b15803cb0f609e2717a80c90
cdude <cdude996@gmail.com>
2019-11-06 10:02:48 -0500

    revised answer

--- chapter07/ex13.c
+++ chapter07/ex13.c
@@ -3,60 +3,59 @@
  * indicating whether the array is to be sorted in ascending or descending
  * order. Then modify the sort() algorithm to correctly sort the array into the
  * indicated order.
+ *
  * By Faisal Saadatmand
  */
 
+/*
+ * Note: this exercise could be made more succinct and efficient by using
+ * features that have not been discussed at this point in the book. See
+ * ext13a.c for an implementation.
+ */
+
 #include <stdio.h>
+#include <stdbool.h>
 
-void sort(int a[], int n, char s)
+/* functions */
+void sort(int [], int, bool);
+
+void sort(int a[], int n, bool descending)
 {
-	int i, j, temp;
+	int i, j, temp, cond;
 
-	if (s == 'a') {
-		for (i = 0; i < n - 1; ++i) 
-			for (j = i + 1; j < n; ++j)
-				if (a[i] > a[j]) {
-					temp = a[i];
-					a[i] = a[j];
-					a[j] = temp;
-				}
-	} else if (s == 'd') {
-		for (i = 0; i < n - 1; ++i)
-			for (j = i + 1; j < n; ++j)
-				if (a[i] < a[j]) {
-					temp = a[i];
-					a[i] = a[j];
-					a[j] = temp;
-				}
-	} else
-		printf("\n\n %c is an invalid sort criteria\n", s);
+	for (i = 0; i < n - 1; ++i) 
+		for (j = i + 1; j < n; ++j) {
+			cond = (descending) ? a[i] < a[j] : a[i] > a[j];
+			if (cond) {
+				temp = a[i];
+				a[i] = a[j];
+				a[j] = temp;
+			}
+		}
 }
 
 int main(void) 
 {
 	int i;
 	int array[16] = {34, -5, 6, 0, 12, 100, 56, 22,
-                    44, -3, -9, 12, 17, 22, 6, 11};
+                     44, -3, -9, 12, 17, 22, 6, 11};
 
-	void sort(int a[], int n, char s);
 
-	printf("The array before the sort:\n");
+	printf("\nBefore sorting:\n");
 	for (i = 0; i < 16; ++i)
-		printf("%i ", array[i]);
+		printf(" %i", array[i]);
 
-	sort(array, 16, 'a');
-
-	printf("\n\nThe array after the sort in ascending order:\n");
+	sort(array, 16, 0);
+	printf("\n\nSorted in ascending order:\n");
 	for (i = 0; i < 16; ++i)
-		printf("%i ", array[i]);
+		printf(" %i", array[i]);
 
-	sort(array, 16, 'd');
-
-	printf("\n\nThe array after the sort in descending order:\n");
+	sort(array, 16, 1);
+	printf("\n\nSorted in descending order:\n");
 	for (i = 0; i < 16; ++i)
-		printf("%i ", array[i]);
+		printf(" %i", array[i]);
 
-	printf("\n");
+	printf("\n\n");
 
 	return 0;
 }
