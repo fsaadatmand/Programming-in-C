64450af716e0fa5fa49ad821827191990ca9b625
cdude <cdude996@gmail.com>
2019-11-06 11:10:43 -0500

    revise answer

--- chapter07/ex6.c
+++ chapter07/ex6.c
@@ -3,35 +3,44 @@
  * precision argument and returns the result as a double precision value. Be
  * certain to change the value of the variable epsilon to reflect the fact that
  * double precision variables are now being used.
+ *
  * By Faisal Saadatmand
  */
 
 #include <stdio.h>
 
+/* functions */
+double absoluteValue(double x);
+double squareRoot(double x, const double);
+
 /* Function to calculate the absolute value of a number */
-float absoluteValue(float x)
+double absoluteValue(double x)
 {
 	if (x < 0)
 		x = -x;
-	return (x);
+
+	return x;
 }
 
 /* Function to compute the square root of a number */
-double squareRoot(float x, const double epsilon)
+double squareRoot(double x, const double epsilon)
 {
-	double guess         = 1.0;
+	double guess = 1.0;
 
 	do {
 		guess = (x / guess + guess) / 2.0;
-	} while (absoluteValue((guess * guess) / x) >= epsilon); 
+	} while (absoluteValue(guess * (guess / x)) >= epsilon); 
+
 	return guess;
 }
 
 int main (void) 
 {
-	printf("squareRoot(2.0) = %.2f\n", squareRoot(2.0, 1.00001));
-	printf("squareRoot(144.0) = %.2f\n", squareRoot(144.0, 1.00001));
-	printf("squareRoot(17.5) = %.2f\n", squareRoot(17.5, 1.00001));
+	const double epsilon = 1.00001;
+
+	printf("squareRoot(2.0) = %f\n", squareRoot(2.0, epsilon));
+	printf("squareRoot(144.0) = %f\n", squareRoot(144.0, epsilon));
+	printf("squareRoot(17.5) = %f\n", squareRoot(17.5, epsilon));
 
 	return 0;
 }
--- chapter07/ex8.c
+++ chapter07/ex8.c
@@ -1,15 +1,19 @@
 /*
  * 8. An equation of the form
+ *
  *		ax2 + bx + c = 0
+ *
  * is known as a quadratic equation. The values of a, b, and c in the preceding
  * example represent constant values. So
+ *
  * 		4x2 - 17x - 15 = 0
  *
  * represents a quadratic equation where a = 4, b = −17, and c = −15.  The
  * values of x that satisfy a particular quadratic equation, known as the roots
  * of the equation, can be calculated by substituting the values of a, b, and c
  * into the following two formulas:
- * 		SEE TEXTBOOK
+ *
+ * 		SEE TEXTBOOK FOR FORMULA
  *
  * If the value of b2−4ac, called the discriminant, is less than zero, the
  * roots of the equation, x1 and x2, are imaginary numbers.
@@ -26,18 +30,24 @@
 
 #include <stdio.h>
 
+/* functions */
+double absoluteValue(double);
+double squareRoot(double);
+
 /* Function to calculate the absolute value of a number */
-float absoluteValue(float x)
+double absoluteValue(double x)
 {
 	if (x < 0)
 		x = -x;
-	return (x);
+
+	return x;
 }
 
 /* Function to compute the square root of a number */
-double squareRoot(double x, const double epsilon)
+double squareRoot(double x)
 {
-	double guess         = 1.0;
+	const double epsilon = 1.00001;
+	double guess = 1.0;
 
 	do {
 		guess = (x / guess + guess) / 2.0;
@@ -45,40 +55,26 @@ double squareRoot(double x, const double epsilon)
 	return guess;
 }
 
-
 int main(void) 
 {
-	
-	int       	a, b, c, x1, x2, discriminant, leftVar, rightVar;
-	float       absoluteValue(float x);
-	double      squareRoot(double x, const double epsilon);
+	double a, b, c, x1, x2, discriminant;
 
+	printf("Enter values for a, b and c: ");
+	scanf("%lf %lf %lf", &a, &b, &c);
 
-	printf("Enter value for a: ");
-	scanf("%i", &a);
-
-	printf("Enter value for b: ");
-	scanf("%i", &b);
-
-	printf("Enter value for c: ");
-	scanf("%i", &c);
+	if (a == 0) {
+		printf("The value of (a) must be a nonzero constant\n");
+		return -1;
+	}
 
 	discriminant = (b * b) - (4 * a * c);
-	
-	if (a == 0)
-		printf("division by zero\n");
 
-	else if (discriminant < 0)
-		printf("Values of x1 and x2 are imaginary.\n");
-
+	if (discriminant < 0)
+		printf("The roots of the equation, x1 and x2, are imaginary.\n");
 	else { 
-		leftVar = -b / (2 * a);
-		rightVar = squareRoot(discriminant, 1.00001) / (2 * a);
-
-		x1 = leftVar + rightVar;
-		x2 = leftVar - rightVar;
-
-		printf("x1 = %i, x2 = %i\n", x1, x2);
+		x1 = (-b + squareRoot(discriminant)) / (2 * a);
+		x2 = (-b - squareRoot(discriminant)) / (2 * a);
+		printf("x1 = %g, x2 = %g\n", x1, x2);
 	}	
 	return 0;
 }
