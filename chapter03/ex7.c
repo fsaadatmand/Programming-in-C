063d1038fea7b6f46ba621cb702643c0c5354831
cdude <cdude996@gmail.com>
2019-11-05 23:47:32 -0500

    fix whitespace

--- chapter03/ex7.c
+++ chapter03/ex7.c
@@ -1,8 +1,10 @@
 /* 
  * 7. Write a program that evaluates the following expression and displays the
  * results (remember to use exponential format to display the result):
+ *
  *     (3.31 x 10^-8 x  2.01 x 10^-7) / (7.16 x 10^-6 + 2.01 x 10^-8) 
- * by Faisal Saadatmand
+ *
+ * By Faisal Saadatmand
  */
 
 #include <stdio.h>
