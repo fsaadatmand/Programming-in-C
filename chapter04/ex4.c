7f2227639469830a4ee973e7d56b4f3db00f0917
cdude <cdude996@gmail.com>
2019-11-05 22:42:27 -0500

    simplify code

--- chapter04/ex4.c
+++ chapter04/ex4.c
@@ -1,7 +1,10 @@
 /*
  * 4. The factorial of an integer n, written n!, is the product of the
+ *
  *     5!  =  5 x 4 x 3 x 2 x 1  =  120
+ *
  * Write a program to generate and print a table of the first 10 factorials.
+ *
  * by Faisal Saadatmand
  */
 
@@ -9,16 +12,13 @@
 
 int main(void)
 {
-	int i, j;           /* counters for main and nested loop */
-	int	factorial;      /* factorial value */
+	int i, j, factorial;
 
-	factorial = 1;
-	
-	for (i = 1; i <=10; ++i) {
-		for (j = 1; j <= i; ++j)
-			factorial = factorial * j;
-		printf("%2i! = %d \n", i, factorial);
+	for (i = 1; i < 11; ++i) {
 		factorial = 1;
+		for (j = i; j > 0; --j)
+			factorial *= j;
+		printf("%2i! = %d\n", i, factorial);
 	}
 	return 0;
 }
