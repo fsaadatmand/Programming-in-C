a873b5c71e954267968e1fc6e998f49a93ca02b4
cdude <cdude996@gmail.com>
2019-11-05 23:08:58 -0500

    add name

--- chapter04/ex8.c
+++ chapter04/ex8.c
@@ -2,6 +2,7 @@
  * 8. Program 4.5 allows the user to type in only five different numbers.
  * Modify that program so that the user can type in the number of triangular
  * numbers to be calculated.
+ *
  * By Faisal Saadatmand
  */
 
@@ -15,15 +16,11 @@ int main(void)
 	scanf("%i", &nTriangular);
 
 	for (counter = 1; counter <= nTriangular; ++counter) {
-
 		printf("What triangular number do you want? ");
 		scanf("%i", &number);
-
 		triangularNumber = 0;
-		
 		for (n = 1; n <= number; ++n)
 			triangularNumber += n;
-
 		printf("Triangular number %i is %i\n\n", number, triangularNumber);
 	}
 	return 0;
