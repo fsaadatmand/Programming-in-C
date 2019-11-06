577988ba521ac341ea17558d883024a93b4af067
cdude <cdude996@gmail.com>
2019-11-05 23:28:41 -0500

    minor tweaks

--- chapter04/ex11.c
+++ chapter04/ex11.c
@@ -2,6 +2,7 @@
  * 11. Write a program that calculates the sum of the digits of an integer. For
  * example, the sum of the digits of the number 2155 is 2 + 1 + 5 + 5 or 13.
  * The program should accept any arbitrary integer typed in by the user.
+ *
  * By Faisal Saadatmand
  */
 
@@ -9,21 +10,20 @@
 
 int main(void)
 {
-	long long int number, sum, right_digit, counter;
+	long long int number, sum;
+	int right_digit;
 
-	printf("Enter a number to calculate its sum: ");
+	printf("Enter a number to calculate the sum of digits: ");
 	scanf("%lli", &number);
 
 	sum = 0;
-	counter = number;
-
-	while (counter != 0) {
-		right_digit = counter % 10;
+	while (number != 0) {
+		right_digit = number % 10;
 		sum += right_digit; 
-		counter /= 10;
+		number /= 10;
 	}
 
-	printf("The sum of %lli is %lli\n", number, sum);
+	printf(" -> %lli\n", sum);
 
 	return 0;
 }
