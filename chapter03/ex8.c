ce345cb1611b0fed549fb7d763b81bc212a02c81
cdude <cdude996@gmail.com>
2019-11-05 23:51:51 -0500

    minor tweaks

--- chapter03/ex8.c
+++ chapter03/ex8.c
@@ -1,13 +1,17 @@
 /* 
  * 8. To round off an integer i to the next largest even multiple of another
  * integer j, the following formula can be used:
+ *
  *		Next_multiple = i + j - i % j
+ *
  * For example, to round off 256 days to the next largest number of days evenly
  * divisible by a week, values of i = 256 and j = 7 can be substituted into the
  * preceding formula as follows:
+ *
  * 		Next_multiple    = 256 + 7 - 256 % 7
  *                       = 256 + 7 - 4
  *                       = 259
+ *
  * Faisal Saadatmand 
  */
 
@@ -15,11 +19,12 @@
 
 int main(void)
 {
-	int i, j;
+	int Next_multiple, i, j;
 
 	i = 256, j = 7;
-
-	printf("%d next even multiple for %d is %d\n", i, j, i + j - i % j);
+	Next_multiple = i +j - i % j;
+	printf("%d days next multiple evenly divisible by %d is %d\n",
+			i, j, Next_multiple);
 
 	return 0;
 }
