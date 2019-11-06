cbff70e34bcbfad00df2b042eb91505d6fb5e940
cdude <cdude996@gmail.com>
2019-11-05 23:10:26 -0500

    fix whitespace

--- chapter04/ex9a.c
+++ chapter04/ex9a.c
@@ -2,6 +2,7 @@
  * 9. Rewrite Programs 4.2 through 4.5, replacing all uses of the for statement
  * with equivalent while statements. Run each program to verify that both
  * versions are identical.
+ *
  * By Faisal Saadatmand
  */
 
@@ -12,14 +13,13 @@ int main(void)
 	int n, triangularNumber;
 
 	triangularNumber = 0;
-	n = 1;
 
+	n = 1;
 	while (n <= 200) {	
 		triangularNumber = triangularNumber + n;
 		++n;
 	}
 	printf("The 200th tiangular number is %i\n", triangularNumber);
-
 
 	return 0;
 }
