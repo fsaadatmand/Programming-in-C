da7839316dabdeedf52d0d0662e7915485a5fd1a
cdude <cdude996@gmail.com>
2019-11-06 08:40:48 -0500

    fix whitespace

--- chapter02/ex2.c
+++ chapter02/ex2.c
@@ -4,6 +4,7 @@
  *    2. main() is where program execution begins.
  *    3. Opening and closing braces enclose program statements in a routine.
  *    4. All program statements must be terminated by a semicolon.
+ *
  * By Faisal Saadatmand
 */
 
--- chapter02/ex4.c
+++ chapter02/ex4.c
@@ -1,6 +1,7 @@
 /* 
  * 4. Write a program that subtracts the value 15 from 87 and displays the
  * result, together with an appropriate message, at the terminal.
+ *
  * By Faisal Saadatmand
  */
 
