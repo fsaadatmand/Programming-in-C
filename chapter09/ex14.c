c0756e4d71a69a18116865659a98196973622215
cdude <cdude996@gmail.com>
2019-12-19 12:30:12 -0500

    revise answer
    Rewrote intToStr so it could handle INT_MIN. Removed user input from
    main.

--- chapter09/ex14.c
+++ chapter09/ex14.c
@@ -8,51 +8,82 @@
 
 #include <stdio.h>
 #include <stdbool.h>
+#include <limits.h>
 
-#define MAXLEN 20 
+#define MAXLEN 100 
 
+/* functions */
 void intToStr(int, char []);
+double absoluteValue(double);
+int stringLength(const char []);
+void reverse(char []);
 
+/* intToStr: convert number from an int to a string. Store the result in str */
 void intToStr(int number, char str[])
 {
-	int i, rNumber;
+	int i, digit;
 	bool negative = false;
 
-	/* handle negative numbers */
-	if (number < 0) {
-		number = -number;
+	if (number < 0) /* remember negative sign */
 		negative = true;
-	}
 
-	/* reverse the number */
-	rNumber = 0;
-	while (number > 0) {
-		rNumber = rNumber * 10 + number % 10;
-		number /= 10;
-	}
-
-	/* copy reversed number into str */
 	i = 0;
-	if (negative)
-		str[i++] = '-';
+	do { /* use do while loop to account for the number 0 */
+		digit = absoluteValue(number % 10); /* extract right digit */
+		str[i++] = digit + '0'; /* store as char in str */
+		number /= 10; /* delete digit */
+	} while (number != 0);
 
-	while (rNumber > 0) {
-		str[i++] = rNumber % 10 + '0';
-		rNumber /= 10;
-	}
+	if (negative == true) /* insert negative sign */
+		str[i++] = '-';
 	str[i] = '\0';
+
+	/* reverse the string */
+	reverse(str);
+}
+
+/* absoluteValue: return the absolute value of x */
+double absoluteValue(double x)
+{
+	return (x < 0) ? -x : x;
+}
+
+/* reverse: reverse the order of characters in string s */
+void reverse(char s[])
+{
+	int c, i, j;
+
+	for (i = 0, j = stringLength(s) - 1; i < j; ++i, --j) {
+		c = s[i];
+		s[i] = s[j];
+		s[j] = c;
+	}
+}
+
+/* stringLength: return the length of string s */
+int stringLength(const char s[])
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; ++i)
+		;
+	return i;
 }
 
 int main(void) 
 {
-	int intValue;
-	char string[MAXLEN];
+	char str[MAXLEN];
 
-	printf("Enter integer value: ");
-	scanf("%i", &intValue);
-
-	intToStr(intValue, string);
-	printf("%s\n", string);
+	intToStr(42, str);
+	printf("%s\n", str);
+	intToStr(-42, str);
+	printf("%s\n", str);
+	intToStr(INT_MAX, str);
+	printf("%s\n", str);
+	intToStr(INT_MIN, str);
+	printf("%s\n", str);
+	intToStr(0, str);
+	printf("%s\n", str);
 
 	return 0;
 }
