745497d02c6914b0b8bf35faedc9fc5465a930a3
cdude <cdude996@gmail.com>
2019-12-19 10:36:21 -0500

    fix bug
    Fixed a bug in the for loop that would return -1 even if there were a
    match at the end of the string s.

--- chapter09/ex5.c
+++ chapter09/ex5.c
@@ -20,19 +20,19 @@
 
 int findString(const char [], const char []);
 
-/* findString: to determine if one character string exists inside another
- * string */
-int findString(const char source[], const char sought[])
+/* findString: return string t index position in string s if found, otherwise
+ * return -1 */
+int findString(const char s[], const char t[])
 {
 	int i, j, k;
 
-	for (i = 0; source[i] != '\0'; ++i) {
-		for (j = i, k = 0; source[j] == sought[k]; ++j, ++k)
+	for (i = 0; s[i] != '\0'; ++i) {
+		for (j = i, k = 0; s[j] != '\0' && s[j] == t[k]; ++j, ++k)
 			;
-		if (sought[k] == '\0') // match is found if sought reached end
-			return i; // return match position in source
+		if (t[k] == '\0') /* found match */
+			return i; /* return position of match */
 	}
-	return -1;
+	return -1; /* match not found */
 }		
 
 int main(void) 
@@ -41,6 +41,7 @@ int main(void)
 	printf("%i\n", findString("a chatterbox", "car"));
 	printf("%i\n", findString("a chatterbox", "ter"));
 	printf("%i\n", findString("a chatterbox", "a chatterboxer"));
+	printf("%i\n", findString("a chatterbox", "x"));
 
 	return 0;
 }
